8c2722774b7cd91cc7cf5d8fe8e4b6b5771af52b
ojas <59116312+ojasjain24@users.noreply.github.com>
2023-01-04 17:00:33 +0530

    multi_class

--- Assets/Scripts/endRing.cs
+++ Assets/Scripts/endRing.cs
@@ -3,14 +3,29 @@ using UnityEngine;
 public class endRing : MonoBehaviour
 {
     public float speed = 100;
+    public GameObject[] objects;
     public Rigidbody arm;
     private float baseYRot = 0.0f;
     private bool isCollided = false;
+    private bool isBack = false;
+    private Vector3 dir = new Vector3(0, 0, 0);
+
+    private float angle = 0;
 
     private GameObject myCollider;
+
+
     void Update()
     {
-        if (isCollided) ProcessMovement();
+        arm.GetComponent<Transform>().localPosition = new Vector3(0.001289082f, 0.253f, -0.004275528f);
+
+        if (isCollided) ProcessMovement(angle, dir);
+        else if (isBack) ProcessBackMovement();
+        else
+        {
+            arm.GetComponent<Transform>().localEulerAngles = new Vector3(arm.GetComponent<Transform>().localEulerAngles.x, 0.0f, 0.0f);
+
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -19,19 +34,52 @@ public class endRing : MonoBehaviour
         if (collision.collider.tag == "subject")
         {
             myCollider = collision.gameObject;
+            dir = new Vector3(0, 0, -2);
+            angle = 90.0f;
+            isCollided = true;
+        }
+        else if (collision.collider.tag == "cyl")
+        {
+            myCollider = collision.gameObject;
+            dir = new Vector3(0, 0, -2);
+            angle = 30.0f;
+            isCollided = true;
+        }
+        else if (collision.collider.tag == "cone")
+        {
+            myCollider = collision.gameObject;
+            dir = new Vector3(2, 0, 0);
+            angle = 120.0f;
             isCollided = true;
         }
     }
 
-    void ProcessMovement()
+    void ProcessMovement(float angle, Vector3 dir)
     {
-        baseYRot -= 20 * Time.deltaTime * 1.0f;
-        baseYRot = Mathf.Clamp(baseYRot, -90.0f, 90.0f);
+        baseYRot -= 40 * Time.deltaTime * 1.0f;
+        baseYRot = Mathf.Clamp(baseYRot, -180.0f, 180.0f);
         arm.GetComponent<Transform>().localEulerAngles = new Vector3(arm.GetComponent<Transform>().localEulerAngles.x, baseYRot, arm.GetComponent<Transform>().localEulerAngles.z);
-        if (baseYRot <= -90.0f)
+        if (baseYRot <= -angle)
         {
-            myCollider.GetComponent<Rigidbody>().transform.Translate(0, 0, -2);
+            myCollider.GetComponent<Rigidbody>().transform.Translate(dir);
             isCollided = false;
+            ProcessBackMovement();
+        }
+    }
+
+    void ProcessBackMovement()
+    {
+        isBack = true;
+        baseYRot += 50 * Time.deltaTime * 1.0f;
+        baseYRot = Mathf.Clamp(baseYRot, -180.0f, 180.0f);
+        arm.GetComponent<Transform>().localEulerAngles = new Vector3(arm.GetComponent<Transform>().localEulerAngles.x, baseYRot, arm.GetComponent<Transform>().localEulerAngles.z);
+        if (baseYRot >= 0.00f)
+        {
+            baseYRot = 0.0f;
+            arm.GetComponent<Transform>().localEulerAngles = new Vector3(arm.GetComponent<Transform>().localEulerAngles.x, baseYRot, 0.0f);
+            isBack = false;
+            int i = Random.Range(0, objects.Length);
+            Instantiate(objects[i], new Vector3(10, 5, 20), new Quaternion(0, 0, 0, 0));
         }
     }
 
