055f06da4736b931f25b6541d1678195410c23ac
ojas <59116312+ojasjain24@users.noreply.github.com>
2023-01-24 19:24:15 +0530

    leap-cantroller-rotrix

---
+++ Assets/Scripts/leapRotrixCantroller.cs
@@ -0,0 +1,26 @@
+using Leap;
+using Leap.Unity;
+using UnityEngine;
+
+public class leapRotrixCantroller : MonoBehaviour
+{
+    public LeapServiceProvider LeapServiceProvider;
+    public Rigidbody arm;
+
+    void Update()
+    {
+        for (int i = 0; i < LeapServiceProvider.CurrentFrame.Hands.Count; i++)
+        {
+            Hand _hand = LeapServiceProvider.CurrentFrame.Hands[i];
+
+            Arm _arm = _hand.Arm;
+
+            float _armLength = _arm.Length;
+            float _armWidth = _arm.Width;
+            Vector3 _elbowPosition = _arm.ElbowPosition;
+
+            arm.GetComponent<Transform>().localEulerAngles = new Vector3(arm.GetComponent<Transform>().localEulerAngles.x, _hand.PalmPosition.y*500, arm.GetComponent<Transform>().localEulerAngles.z);
+
+        }
+    }
+}
