52634533d212d4236d411a733285acf1f3c08dc5
ojasjain24 <ojas24012002jain@gmail.com>
2023-02-25 22:27:43 +0530

    softgripper

--- Assets/Scripts/LeapRotricsController.cs
+++ Assets/Scripts/LeapRotricsController.cs
@@ -5,8 +5,8 @@ using System.IO.Ports;
 
 public class LeapRotricsController : MonoBehaviour
 {
-    private SerialPort data_stream = new SerialPort("COM8", 115200);
-
+    private SerialPort data_stream = new SerialPort("COM9", 115200);
+    bool isClosed = false;
     void Start()
     {
         data_stream.Open();
@@ -19,14 +19,47 @@ public class LeapRotricsController : MonoBehaviour
     {
         for (int i = 0; i < LeapServiceProvider.CurrentFrame.Hands.Count; i++)
         {
-            Hand _hand = LeapServiceProvider.CurrentFrame.Hands[i];
-
+            Hand _hand = Hands.Left ?? Hands.Right;
             Arm _arm = _hand.Arm;
-            float Y = _hand.PalmPosition.y * 500;
-            data_stream.WriteLine("G0 Y" + Y);
+            float Y = _hand.WristPosition.y * 500;
+            float X = _hand.WristPosition.x * 500;
+            float Z = _hand.WristPosition.z * 500;
+            if (!_hand.Fingers[0].IsExtended && !_hand.Fingers[1].IsExtended && !_hand.Fingers[2].IsExtended && !_hand.Fingers[3].IsExtended && !_hand.Fingers[4].IsExtended)
+            {
+                fistClose(_hand);
+
+            }
+            if (_hand.Fingers[0].IsExtended && _hand.Fingers[1].IsExtended && !_hand.Fingers[2].IsExtended && !_hand.Fingers[3].IsExtended && !_hand.Fingers[4].IsExtended)
+            {
+                fistOpen();
+            }
+            data_stream.WriteLine("G1 X" + X);
+            data_stream.WriteLine("G1 Y" + Y);
+            data_stream.WriteLine("G1 Z" + Z);
         }
     }
 
+    public void fistClose(Hand _hand)
+    {
+        if (data_stream.IsOpen && !isClosed)
+        {
+            print("CLOSE");
+
+            data_stream.WriteLine("M1001");
+            isClosed = true;
+        }
+    }
+
+    public void fistOpen()
+    {
+
+        if (data_stream.IsOpen && isClosed)
+        {
+            print("OPEN");
+            data_stream.WriteLine("M1002");
+            isClosed = false;
+        }
+    }
     public void moveToHome()
     {
         if (data_stream.IsOpen)
--- Assets/Scripts/leapRotrixModelCantroller.cs
+++ Assets/Scripts/leapRotrixModelCantroller.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class leapRotrixModelCantroller : MonoBehaviour
 {
     public LeapServiceProvider LeapServiceProvider;
-    public Rigidbody arm;
+    public Rigidbody rotricsBase;
+    public Rigidbody rotricsLowerBone;
+    public Rigidbody rotricsUpperBone;
 
+    public Rigidbody rotricsGripper;
     void Update()
     {
         for (int i = 0; i < LeapServiceProvider.CurrentFrame.Hands.Count; i++)
@@ -17,9 +20,32 @@ public class leapRotrixModelCantroller : MonoBehaviour
 
             float _armLength = _arm.Length;
             float _armWidth = _arm.Width;
-            Vector3 _elbowPosition = _arm.ElbowPosition;
 
-            arm.GetComponent<Transform>().localEulerAngles = new Vector3(arm.GetComponent<Transform>().localEulerAngles.x, _hand.PalmPosition.y * 500, arm.GetComponent<Transform>().localEulerAngles.z);
+            Vector3 _wristPosition = _arm.WristPosition;
+            Vector3 _gripperPosition = rotricsGripper.GetComponent<Transform>().localPosition;
+            float Xval = (_wristPosition.x) * 5;
+            float Yval = (_wristPosition.y) * 5;
+            float Zval = (_wristPosition.z) * 5;
+            Vector3 _elbowAngle = _arm.ElbowPosition - _arm.WristPosition;
+            float l1 = rotricsLowerBone.GetComponent<MeshFilter>().mesh.bounds.extents.y;
+            float l2 = rotricsUpperBone.GetComponent<MeshFilter>().mesh.bounds.extents.y;
+            // print(Xval + ", " + Yval + ", " + l1 + ", " + l2);
+            float cosVal = Mathf.Clamp((((Xval * Xval) + (Yval * Yval) - (l1 * l1) - (l2 * l2)) / (2 * l1 * l2)), -1, +1);
+            float beta = Mathf.Acos(cosVal);
+            float theta = Mathf.Asin((Xval * l2 * Mathf.Sin(beta) - (l1 * Yval + l2 * Yval * Mathf.Cos(beta))) / (l1 * l1 + l2 * l2 + 2 * l1 * l2 * Mathf.Cos(beta)));
+
+            float alpha = theta + beta;
+            float baseAngle = Mathf.Atan(Zval / Xval) * 20;
+            // print("THETA : " + theta * 180 / Mathf.PI);
+
+
+
+            rotricsLowerBone.GetComponent<Transform>().localRotation = Quaternion.Euler(-theta * 180 / Mathf.PI, 0, 0);
+
+            rotricsUpperBone.GetComponent<Transform>().localRotation = Quaternion.Euler(alpha * 180 / Mathf.PI, 0, 0);
+
+            rotricsBase.GetComponent<Transform>().localEulerAngles = new Vector3(-90, 0, baseAngle * 180 / Mathf.PI);
+            // robotBase.localEulerAngles = new Vector3(robotBase.localEulerAngles.x, baseYRot, robotBase.localEulerAngles.z);
 
         }
     }
